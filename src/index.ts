c1c0ece2735c1e3e373f09b8f80bc4ce19cbbf59
musashibot <musashi@musashibot.com>
2020-04-19 21:17:04 -0700

    added login page and logout

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import session from 'express-session';
 import passport from 'passport';
 const mongoose = require('mongoose');
 import passportLocalMongoose from 'passport-local-mongoose';
-import connectEnsureLogin from 'connect-ensure-login';
+
+import login from './login';
 
 
 const app = express();
@@ -43,49 +44,6 @@ passport.use(UserDetails.createStrategy());
 passport.serializeUser(UserDetails.serializeUser());
 passport.deserializeUser(UserDetails.deserializeUser());
 
-app.post('/login', (req, res, next) => {
-    passport.authenticate('local',
-    (err, user, info) => {
-      if (err) {
-        return next(err);
-      }
+app.use('/', login);
   
-      if (!user) {
-        return res.redirect('/login?info=' + info);
-      }
-  
-      req.logIn(user, function(err) {
-        if (err) {
-          return next(err);
-        }
-  
-        return res.redirect('/');
-      });
-  
-    })(req, res, next);
-  });
-  
-  app.get('/login',
-    (req, res) => res.sendFile('html/login.html',
-    { root: __dirname })
-  );
-  
-  app.get('/',
-    connectEnsureLogin.ensureLoggedIn(),
-    (req, res) => res.sendFile('html/index.html', {root: __dirname})
-  );
-  
-  app.get('/private',
-    connectEnsureLogin.ensureLoggedIn(),
-    (req, res) => res.sendFile('html/private.html', {root: __dirname})
-  );
-  
-  app.get('/user',
-    connectEnsureLogin.ensureLoggedIn(),
-    (req, res) => res.send({user: req.user})
-  );
-
-
-app.get('/', (req, res) => res.send('hello world'));
-
 app.listen(port, () => console.log(`Example app listening on port:${port}`));
---
+++ src/login.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import connectEnsureLogin from 'connect-ensure-login';
+import passport from 'passport';
+
+const router = express.Router();
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local',
+    (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+  
+      if (!user) {
+        return res.redirect('/login?info=' + info);
+      }
+  
+      req.logIn(user, function(err) {
+        if (err) {
+          return next(err);
+        }
+  
+        return res.redirect('/');
+      });
+  
+    })(req, res, next);
+  });
+  
+  router.get('/login',
+    (req, res) => res.sendFile('html/login.html',
+    { root: process.cwd() })
+  );
+  
+  router.get('/',
+    connectEnsureLogin.ensureLoggedIn(),
+    (req, res) => res.sendFile('html/index.html', {root: process.cwd()})
+  );
+  
+  router.get('/private',
+    connectEnsureLogin.ensureLoggedIn(),
+    (req, res) => res.sendFile('html/private.html', {root: process.cwd()})
+  );
+  
+  router.get('/user',
+    connectEnsureLogin.ensureLoggedIn(),
+    (req, res) => res.send({user: req.user})
+  );
+
+  router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  /*
+  UserDetails.register({username:'paul', active: false}, 'paul');
+  UserDetails.register({username:'jay', active: false}, 'jay');
+  UserDetails.register({username:'roy', active: false}, 'roy');
+  */
+ export = router;
