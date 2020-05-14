c99ee27f92de5e22398ce9403ea7983336c2f0dd
musashibot <musashi@musashibot.com>
2020-05-13 21:45:23 -0700

    refactored code, added util

--- src/routes/rb.ts
+++ src/routes/rb.ts
@@ -1,128 +1,76 @@
 import express, { Request, Response } from 'express';
 import connectEnsureLogin from 'connect-ensure-login';
-import crypto from 'crypto';
-import path from 'path';
 import multer from 'multer';
-
-
-import rbModel from '../models/rb';
-import ratingsModel from '../models/rb_ratings';
-
-import RUtils from '../utils/route_utils';
-import userModel from '../models/user';
-
 import permissions from '../utils/permissions';
 
-const routeUtils = new RUtils();
+import RUtils from '../utils/route_utils';
 
+const routeUtils = new RUtils();
 const router = express.Router();
 
 router.get('/rootbeer',
     connectEnsureLogin.ensureLoggedIn('/'),
     permissions(['king', 'rr']),
-    async (req: Request, res: any, next: any) => {
-        const { user }: any = req;
-
-        res.render('pages/rb/home', { user });
+    async (req: Request, res: any) => {
+        res.render('pages/rb/home', { user: routeUtils.getUser(req) });
     }
 );
 
 router.post('/rb_search',
     connectEnsureLogin.ensureLoggedIn('/'),
     permissions(['king', 'rr']),
-    async (req: any, res: Response) => {
-        const { user }: any = req;
+    async (req: Request, res: Response) => {
+        const search = routeUtils.makeRegex(req.body.rb_search);
 
-        let search;
+        let results = await routeUtils.searchRootbeer('name', search); 
+    
+        results = await routeUtils.addUserName(results, 'created_by');
 
-        try {
-            search = new RegExp(req.body.rb_search, 'i');
-        } catch (e) {
-            res.send(e);
-        }
-
-        const results = await rbModel.find({ name: search });
-
-        for (const r of results) {
-            const name = await userModel.findById(r.created_by, 'username');
-            r.created_by = name.username;
-        }
-
-        res.render('pages/rb/display', { user, results });
+        res.render('pages/rb/display', {
+            user: routeUtils.getUser(req),
+            results
+        });
     }
 );
 
 router.get('/rb_create',
     connectEnsureLogin.ensureLoggedIn('/'),
     permissions(['king', 'rr']),
-    (req: any, res: any) => {
-        const { user }: any = req;
-
-        res.render('pages/rb/create', { user });
+    (req: Request, res: Response) => {
+        res.render('pages/rb/create', { user: routeUtils.getUser(req) });
     }
 );
 
-
-router.get('/rb/:rb_id', 
+router.get('/rb/:id', 
     connectEnsureLogin.ensureLoggedIn('/'),
     permissions(['king', 'rr']),
     async (req: Request, res: Response) => {
-        const id = req.params.rb_id;
-        const rb = await rbModel.find({ _id: id });
+        const rbId = routeUtils.getParam(req, 'id');
 
-        const ratings = await ratingsModel.find({ rb_id: id });
+        const rb = await routeUtils.getRbById(rbId);
 
-        
-        for (const r of ratings) {
-            const name = await userModel.findById(r.rated_by, 'username');
-            r.rated_by = name.username;
-        }
+        let ratings = await routeUtils.searchRatings('rb_id', rbId);
 
-        const { user }: any = req;
+        ratings = await routeUtils.addUserName(ratings, 'rated_by')
 
-        res.render('pages/rb/view', { user, rb: rb[0], ratings });
+        res.render('pages/rb/view', {
+            user: routeUtils.getUser(req),
+            rb: rb,
+            ratings
+        });
 });
 
-const imgPath = path.join(process.cwd(), 'static', 'rb_imgs');
-    
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, imgPath);
-    },
-    filename: (req, file, cb) => {
-        const shasum = crypto.createHash('md5');
-
-        const hashInput = new Date().getTime() * Math.random();
-
-        shasum.update(`${hashInput}`, 'utf8');
-    
-        const prefix = shasum.digest('hex');
-
-        cb(null, `${prefix}_${file.originalname}`);
-    }
-});
-
-const upload = multer({ storage });
+const upload = multer({ storage: routeUtils.imgStorage() });
 
 router.post(
     '/rb_create',
     connectEnsureLogin.ensureLoggedIn('/'),
     permissions(['king', 'rr']),
     upload.single('rb_image'),
-    async (req: any, res: any) => {
-        const newRbInfo = {
-            name: req.body.rb_brand_name,
-            created: new Date(),
-            created_by: req.user._id,
-            image: `rb_imgs/${req.file.filename}`
-        };
-        
-        try {
-            await rbModel.create(newRbInfo);
-        } catch (e) {
-            return res.send(e);
-        }
-        
+    async (req: Request, res: Response) => {
+        const response = await routeUtils.createRB(req);
+        console.log(response);
+
         res.redirect('/rootbeer');
 });
 
@@ -131,42 +79,28 @@ router.post(
     connectEnsureLogin.ensureLoggedIn('/'),
     permissions(['king', 'rr']),
     upload.single('rb_image'),
-    async (req: any, res: any) => {
-        const updateFields: { name?: string, image?: string } = {};
-        
-        if (req.file && req.file.path) {
-            updateFields.image = `rb_imgs/${req.file.filename}`;
-        }
+    async (req: Request, res: Response) => {
+        const response = await routeUtils.updateRB(req);
+        console.log(response);
 
-        if(req.body && req.body.rb_brand_name) {
-            updateFields.name = req.body.rb_brand_name;
-        }
-
-        if (Object.keys(updateFields).length) {
-            try {
-                const update = await rbModel.updateOne({ _id: req.params.id }, updateFields) 
-            } catch (e) {
-                res.send(e);
-            }
-        }
-        
         res.redirect(`/rb/${req.params.id}`);
 });
 
 router.get('/rb_mine',
     connectEnsureLogin.ensureLoggedIn('/'),
     permissions(['king', 'rr']),
-    async (req: any, res: Response) => {
-        const { user }: any = req;
+    async (req: Request, res: Response) => {
+        let results = await routeUtils.searchRootbeer(
+            'created_by',
+            routeUtils.getUserId(req)
+        );
+    
+        results = await routeUtils.addUserName(results, 'created_by');
 
-        const results = await rbModel.find({ created_by: user._id });
-
-        for (const r of results) {
-            const name = await userModel.findById(r.created_by, 'username');
-            r.created_by = name.username;
-        }
-
-        res.render('pages/rb/display', { user, results });
+        res.render('pages/rb/display', {
+            user: routeUtils.getUser(req),
+            results
+        });
     }
 );
 
@@ -174,16 +108,13 @@ router.get('/rb_every',
     connectEnsureLogin.ensureLoggedIn('/'),
     permissions(['king', 'rr']),
     async (req: any, res: Response) => {
-        const { user }: any = req;
+        let results = await routeUtils.getEveryRB();
+        results = await routeUtils.addUserName(results, 'created_by'); 
 
-        const results = await rbModel.find();
-
-        for (const r of results) {
-            const name = await userModel.findById(r.created_by, 'username');
-            r.created_by = name.username;
-        }
-
-        res.render('pages/rb/display', { user, results });
+        res.render('pages/rb/display', {
+            user: routeUtils.getUser(req),
+            results
+        });
     }
 );
 
--- src/utils/route_utils.ts
+++ src/utils/route_utils.ts
@@ -1,9 +1,137 @@
+import { Request, Response } from 'express';
+import path from 'path';
+import safe from 'safe-regex';
+import multer from 'multer';
+import crypto from 'crypto';
+import escapeString from 'js-string-escape';
+import RBModel from '../models/rb';
+import UserModel from '../models/user';
+import RatingsModel from '../models/rb_ratings';
+import * as I from './interface';
+
+
 class RB {
-    getUser(req: any) {
-        
+    getUser(req: Request)  {
+       return req.user;
     }
 
+    getUserId(req: Request) {
+        const { user }: any = req;
 
+        return user._id;
+    }
+
+    getParam(req: Request, field: string) {
+        return req.params[field];
+    }
+
+    sanitizeRegex(search: string) {
+        const escaped = escapeString(search); 
+
+        if (safe(escaped)) {
+            return escaped;
+        }
+
+        return null;
+    }
+
+    makeRegex(search: string) {
+        const sanitized = this.sanitizeRegex(search);
+
+        if (sanitized) {
+            try {
+                return new RegExp(sanitized, 'i');
+            } catch (e) {
+                return null
+            }
+        }
+
+        return null;
+    }
+
+    searchRootbeer(field: string, search: string | RegExp | null) {
+        return RBModel.find({ [field]: search });
+    }
+
+    getRbById(id: string) {
+        return RBModel.findById(id);
+    }
+
+    getEveryRB() {
+        return RBModel.find({});
+    }
+
+    searchRatings(field: string, search: string | number) {
+        return RatingsModel.find({ [field]: search });
+    }
+
+    async addUserName(objArray: any[], userIdField: string): Promise<any[]> {
+        for (const i of objArray) {
+            const user = await UserModel.findById(i[userIdField], 'username');
+            i[userIdField] = user.username;
+        }
+
+        return objArray;
+    }
+
+    uniqFileName(fileName: string): string {
+        const shasum = crypto.createHash('md5');
+
+        const hashInput = new Date().getTime() * Math.random();
+
+        shasum.update(`${hashInput}`, 'utf8');
+            
+        const prefix = shasum.digest('hex');
+
+        return `${prefix}_${fileName}`;
+    }
+
+    rbInfo(user: any, req: Request) {
+        const newRb: I.RootBeer = {
+            name: req.body.rb_brand_name,
+            created: new Date(),
+            created_by: user._id
+        };
+
+        if (req.file && req.file.filename) {
+            newRb.image = `rb_imgs/${req.file.filename}`;
+        }
+
+        return newRb;
+    }
+
+    createRB(req: Request) {
+        const user = this.getUser(req);
+        const rbInfo = this.rbInfo(user, req);
+        return RBModel.create(rbInfo);
+    }
+
+    updateRB(req: Request) {
+        const updateFields: { name?: string, image?: string } = {};
+        
+        if (req.file && req.file.path) {
+            updateFields.image = `rb_imgs/${req.file.filename}`;
+        }
+
+        if(req.body && req.body.rb_brand_name) {
+            updateFields.name = req.body.rb_brand_name;
+        }
+
+        if (Object.keys(updateFields).length) {
+            return RBModel.updateOne({ _id: req.params.id }, updateFields) 
+        }
+    }
+
+    imgStorage() {
+        return multer.diskStorage({
+            destination: (req: Request, file, cb) => {
+                cb(null, path.join(process.cwd(), 'static', 'rb_imgs'));
+            },
+            filename: (req: Request, file, cb) => {
+                cb(null, this.uniqFileName(file.filename));
+            }
+        });
+    }
 }
 
 export = RB;
