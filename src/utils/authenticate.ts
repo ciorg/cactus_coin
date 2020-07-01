2de9498d31578b12811bab70ea9b9bf1d91cbe61
musashibot <musashi@musashibot.com>
2020-06-30 21:10:07 -0700

    sessions in mongo, helmet

--- src/utils/authenticate.ts
+++ src/utils/authenticate.ts
@@ -1,15 +1,28 @@
 import express from 'express';
 import passport from 'passport';
 import session from 'express-session';
+import MongoDBStore from 'connect-mongodb-session'
 import userModel from '../models/user';
-
 
 const router = express.Router();
 
+const MongoStore = MongoDBStore(session);
+
+const store = new MongoStore({
+    uri: 'mongodb://localhost:27017/MyDatabase',
+    collection: 'mySessions'
+},
+(error) => {console.log('error1', error) }
+);
+
+store.on('error', (error) => console.log('error2', error));
+
 router.use(session({
+    name: 'session',
     secret: 'secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store
 }));
 
 router.use(passport.initialize());
