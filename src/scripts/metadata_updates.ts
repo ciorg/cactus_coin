4897d1681b4d8e1abb0e0c32daa7718fa7fd13fd
ciorg <charlieiorg@gmail.com>
2021-09-27 21:12:26 -0700

    removed console.log

--- src/scripts/metadata_updates.ts
+++ src/scripts/metadata_updates.ts
@@ -2,8 +2,6 @@ import * as metadata from './metadata_utils';
 
 const args: string[] = process.argv;
 
-console.log(args);
-
 async function updateMetadata(args: string[]) {
     const func = args[2];
 
